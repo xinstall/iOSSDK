4829f644560d810890df0e26461772d935a955cf
xinstall <hi@xinstall.com>
2020-07-31 23:39:41 +0800

    add file

---
+++ Classes/XinstallDefault.h
@@ -0,0 +1,18 @@
+//
+//  XinstallDefault.h
+//  XinstallDemo
+//
+//  Created by huawenjie on 2020/5/12.
+//  Copyright © 2020 jiwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef XinstallDefault_h
+#define XinstallDefault_h
+
+extern  NSString  * _Nonnull const XinstallAppKey;
+extern NSString * _Nonnull const XinstallBaseServiceURL;
+extern NSString * _Nonnull const XinstallVersion;
+
+#endif /* XinstallDefault_h */
