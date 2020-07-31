2a620403c8c8084d8199417fe6ba53c81b2f09b9
xinstall <hi@xinstall.com>
2020-07-31 22:54:42 +0800

    1.1.0

--- Classes/XInstallData.h
+++ Classes/XInstallData.h
@@ -2,21 +2,21 @@
 //  XinstallData.h
 //  XInstall
 //
-//  Created by huawenjie on 2020/5/7.
-//  Copyright © 2020 jiwei. All rights reserved.
+//  Created by Xinstall on 2020/5/7.
+//  Copyright © 2020 shu bao. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface XinstallData : NSObject <NSCoding>
+@interface XinstallData : NSObject <NSSecureCoding>
 
 - (instancetype)initWithData:(NSDictionary *)data
                  channelCode:(NSString *)channelCode;
                 
 
-@property (nonatomic, strong) NSDictionary *data;//动态参数
+@property (nonatomic, copy) NSDictionary *data;//动态参数
 @property (nonatomic, copy) NSString *channelCode;//渠道编号
 @property (nonatomic, assign) NSInteger timeSpan;//时间间隔s
 
--- Classes/XInstallDefault.h
+++ Classes/XInstallDefault.h
@@ -2,8 +2,8 @@
 //  XinstallDefault.h
 //  XinstallDemo
 //
-//  Created by huawenjie on 2020/5/12.
-//  Copyright © 2020 jiwei. All rights reserved.
+//  Created by Xinstall on 2020/5/12.
+//  Copyright © 2020 shu bao. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -11,7 +11,7 @@
 #ifndef XinstallDefault_h
 #define XinstallDefault_h
 
-extern  NSString  * _Nonnull const XinstallAppKey;
+extern NSString  * _Nonnull const XinstallAppKey;
 extern NSString * _Nonnull const XinstallBaseServiceURL;
 extern NSString * _Nonnull const XinstallVersion;
 
